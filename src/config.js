9523dc446a578427d85beff74555eb18d26cd579
Hightouch <cetinkaya.ozden@gmail.com>
2025-03-17 23:39:15 +0100

    update startblock

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let config = {
     history: {
         0: {
             // Steem block to start streaming from
-            steemStartBlock: 93902520,
+            steemStartBlock: 93906979,
             // this is the block 0 configuration for mainnet
             accountPriceBase: 20000,
             accountPriceCharMult: 4,
