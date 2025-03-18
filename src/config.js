c02ce6ca8699ae6632415c91108c38ce0ff19529
Burak Akdogan <zeliha.bekcan@gmail.com>
2025-03-18 14:43:31 -0700

    update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let config = {
     history: {
         0: {
             // Steem block to start streaming from
-            steemStartBlock: 93933575,
+            steemStartBlock: 93934595,
             // this is the block 0 configuration for mainnet
             accountPriceBase: 20000,
             accountPriceCharMult: 4,
