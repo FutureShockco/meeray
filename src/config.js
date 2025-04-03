099f3b601d11aa659477f19c0854ce50a0354efd
Hightouch <cetinkaya.ozden@gmail.com>
2025-04-03 02:21:43 +0200

    Refine block synchronization logic and adjust steemBlockDelay for improved performance

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ let config = {
         0: {
             // Steem block to start streaming from
             steemStartBlock: 94311775,
-            steemBlockDelay: 3,
+            steemBlockDelay: 2,
             // this is the block 0 configuration for mainnet
             accountPriceBase: 20000,
             accountPriceCharMult: 4,
