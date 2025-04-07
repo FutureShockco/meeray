0198c56b5fd1ff55fe20ee2310b54ba47a6bff8e
Hightouch <cetinkaya.ozden@gmail.com>
2025-04-07 19:03:30 +0200

    Optimize p2p recovery

--- src/p2p.js
+++ src/p2p.js
@@ -510,8 +510,7 @@ let p2p = {
             p2p.sendJSON(champion, { t: MessageType.QUERY_BLOCK, d: p2p.recovering })
             p2p.recoveringBlocks.push(p2p.recovering)
             logr.debug('query block #' + p2p.recovering + ' -- head block: ' + champion.node_status.head_block)
-            if (p2p.recovering % 2) 
-                setTimeout(() => p2p.recover(), 100)
+            if (p2p.recovering % 2) p2p.recover()
         }
     },
     refresh: (force = false) => {
