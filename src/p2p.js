d477c10880b7ffb7f68cfafdbbae0183d3877258
Hightouch <cetinkaya.ozden@gmail.com>
2025-04-14 17:14:46 +0200

    improve replay

--- src/p2p.js
+++ src/p2p.js
@@ -279,15 +279,24 @@ let p2p = {
                 case MessageType.BLOCK:
                     // a peer sends us a block we requested with QUERY_BLOCK
                     if (!message.d._id || !p2p.recoveringBlocks.includes(message.d._id)) return
-                    for (let i = 0; i < p2p.recoveringBlocks.length; i++)
+                    
+                    // Remove from recovering blocks list
+                    for (let i = 0; i < p2p.recoveringBlocks.length; i++) {
                         if (p2p.recoveringBlocks[i] === message.d._id) {
                             p2p.recoveringBlocks.splice(i, 1)
                             break
                         }
+                    }
 
-                    if (chain.getLatestBlock()._id + 1 === message.d._id)
+                    // Check if we already have this block
+                    if (p2p.recoveredBlocks[message.d._id]) {
+                        logr.debug(`Already received block #${message.d._id}, ignoring duplicate`)
+                        return
+                    }
+
+                    if (chain.getLatestBlock()._id + 1 === message.d._id) {
                         p2p.addRecursive(message.d)
-                    else {
+                    } else {
                         p2p.recoveredBlocks[message.d._id] = message.d
                         p2p.recover()
                     }
@@ -492,25 +501,65 @@ let p2p = {
         if (Object.keys(p2p.recoveredBlocks).length + p2p.recoveringBlocks.length > max_blocks_buffer) return
         if (!p2p.recovering) p2p.recovering = chain.getLatestBlock()._id
 
+        // Find all peers that are ahead of us
         let peersAhead = []
-        for (let i = 0; i < p2p.sockets.length; i++)
-            if (p2p.sockets[i].node_status
-                && p2p.sockets[i].node_status.head_block > chain.getLatestBlock()._id
-                && p2p.sockets[i].node_status.origin_block === config.originHash)
+        for (let i = 0; i < p2p.sockets.length; i++) {
+            if (p2p.sockets[i].node_status &&
+                p2p.sockets[i].node_status.head_block > chain.getLatestBlock()._id &&
+                p2p.sockets[i].node_status.origin_block === config.originHash) {
                 peersAhead.push(p2p.sockets[i])
+            }
+        }
 
         if (peersAhead.length === 0) {
             p2p.recovering = false
             return
         }
 
-        let champion = peersAhead[Math.floor(Math.random() * peersAhead.length)]
-        if (p2p.recovering + 1 <= champion.node_status.head_block) {
-            p2p.recovering++
-            p2p.sendJSON(champion, { t: MessageType.QUERY_BLOCK, d: p2p.recovering })
-            p2p.recoveringBlocks.push(p2p.recovering)
-            logr.debug('query block #' + p2p.recovering + ' -- head block: ' + champion.node_status.head_block)
-            if (p2p.recovering % 2) p2p.recover()
+        // Sort peers by their head block height
+        peersAhead.sort((a, b) => b.node_status.head_block - a.node_status.head_block)
+        
+        // Get the highest block among peers
+        const highestPeerBlock = peersAhead[0].node_status.head_block
+        
+        // Calculate how many blocks we can request in this batch
+        const blocksToRequest = Math.min(
+            max_blocks_buffer - (Object.keys(p2p.recoveredBlocks).length + p2p.recoveringBlocks.length),
+            highestPeerBlock - p2p.recovering
+        )
+
+        if (blocksToRequest <= 0) {
+            p2p.recovering = false
+            return
+        }
+
+        // Request blocks in parallel from different peers
+        for (let i = 0; i < blocksToRequest; i++) {
+            const blockToRequest = p2p.recovering + i + 1
+            
+            // Skip if we're already requesting this block
+            if (p2p.recoveringBlocks.includes(blockToRequest)) {
+                continue
+            }
+            
+            // Skip if we already have this block
+            if (p2p.recoveredBlocks[blockToRequest]) {
+                continue
+            }
+
+            // Choose a random peer for each block to distribute load
+            const randomPeer = peersAhead[Math.floor(Math.random() * peersAhead.length)]
+            p2p.sendJSON(randomPeer, { t: MessageType.QUERY_BLOCK, d: blockToRequest })
+            p2p.recoveringBlocks.push(blockToRequest)
+            logr.debug(`Requesting block #${blockToRequest} from peer ${randomPeer.nodeId} (head: ${randomPeer.node_status.head_block})`)
+        }
+
+        // Update recovering counter
+        p2p.recovering += blocksToRequest
+
+        // If we're still behind, schedule next recovery batch
+        if (p2p.recovering < highestPeerBlock) {
+            setTimeout(() => p2p.recover(), 100) // Small delay between batches
         }
     },
     refresh: (force = false) => {
