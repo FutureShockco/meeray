b62bcfcc32447d99b0f2dd04ef85bf1244b3f49f
Hightouch <cetinkaya.ozden@gmail.com>
2025-09-25 08:12:39 +0200

    fix: update steemStartBlock for development environment to correct value

--- src/config.ts
+++ src/config.ts
@@ -15,7 +15,7 @@ const config = {
     witnessReward: 100000000, // 1 MRY
     farmReward: 100000000, // 1 MRY
     steemChainId: '0000000000000000000000000000000000000000000000000000000000000000',
-    steemStartBlock: process.env.NODE_ENV === 'development' ? 3822500 : 95762370,
+    steemStartBlock: process.env.NODE_ENV === 'development' ? 3889720 : 95762370,
     steemBlockDelay: 1,
     steemBlockMaxDelay: 6,
     b58Alphabet: '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz',
